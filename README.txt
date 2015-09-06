99b662a27bf25921dc27c0922d701b912dbc2ed4
BobbyHo <angela.bobby@gmail.com>
2015-09-06 14:56:06 +0800

    init

---
+++ README.txt
@@ -0,0 +1,7 @@
+A simple plugin for Baidu Tieba Auto Sign
+
+Demo (Originally SChinese, but it has been converted to TChinese)
+http://tieba.bobbyrealms.info
+
+Baidu Tieba Auto Sign Official Website
+http://www.stus8.com/forum.php?mod=forumdisplay&fid=163
---
+++ STChinese_Converter.php
@@ -0,0 +1,28 @@
+﻿<?php
+/*
+
+Plugin Name: SChinese To TChinese Converter
+
+Version: 1.0
+
+Plugin URL: http://about.bobbyrealms.info
+
+Description: 提供簡體轉繁體服務
+
+Author: BobbyHo
+
+Author Email: angela.bobby@gmail.com
+
+Author URL: http://about.bobbyrealms.info
+
+For: 4.0+
+
+*/
+
+if (!defined('SYSTEM_ROOT')) { die('Insufficient Permissions'); } 
+function converter_core() {
+	echo '<script language="JavaScript" src="http://tongwen.openfoundry.org/src/web/tongwen_core.js"></script>';	echo '<script language="JavaScript" src="http://tongwen.openfoundry.org/src/web/tongwen_table_s2t.js"></script>';	echo '<script language="JavaScript" src="http://tongwen.openfoundry.org/src/web/tongwen_table_ps2t.js"></script>';	echo '<script>TongWen.trans2Trad(document);</script>';
+}
+
+addAction('header','converter_core');
+?>
